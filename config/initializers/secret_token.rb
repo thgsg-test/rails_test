cbaed728f927d3dab35f9511d51058055e41c6ff
hgs <hgs@hgs-VirtualBox.(none)>
2014-04-09 01:18:44 +0900

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstUpp::Application.config.secret_key_base = 'cb66dc0d6f58308bf8bfd676ba46030aaf2c52f6ee6dea72f2466da14a2bb4fa6ac3f83c756bff17051803cf41ec26ca518407274f5fc0b2b4d5470edf05c817'
